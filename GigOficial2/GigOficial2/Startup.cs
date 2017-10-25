23f47f03b07aae3a166dd98878f883d919e4b2a1
Dani Llanos <daniel00741@gmail.com>
2017-10-24 20:49:32 -0500

    Creacion de Proyecto

---
+++ GigOficial2/GigOficial2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigOficial2.Startup))]
+namespace GigOficial2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
